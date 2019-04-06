2e545c7252d32350cf4502b88199d421ca802036
Youssef MAHTAT <Youssef.MAHTAT@gmail.com>
2019-04-06 15:08:08 +0100

    add confidential config-file

--- src/main/java/com/cgi/mpw/pocgraph/PocGraphApplication.java
+++ src/main/java/com/cgi/mpw/pocgraph/PocGraphApplication.java
@@ -2,12 +2,13 @@ package com.cgi.mpw.pocgraph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PocGraphApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocGraphApplication.class, args);
+		ApplicationContext context = SpringApplication.run(PocGraphApplication.class, args);
 	}
 
 }
