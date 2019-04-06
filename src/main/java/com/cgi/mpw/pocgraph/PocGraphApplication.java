db0af63e741abc8a601784b792eb57ecb90a404f
Youssef MAHTAT <Youssef.MAHTAT@gmail.com>
2019-04-06 11:44:19 +0100

    first-commit

---
+++ src/main/java/com/cgi/mpw/pocgraph/PocGraphApplication.java
@@ -0,0 +1,13 @@
+package com.cgi.mpw.pocgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocGraphApplication.class, args);
+	}
+
+}
